d8f44807a12ce88690dd9325f22784f4a2b10ea1
Erik Ruud Ulseth <erik.r.u.kodehode@gmail.com>
2023-11-11 01:55:00 +0100

    fine-adjusted line-height settings

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const Modal = ({ isOpen, onClose, onConfirm, children }) => {
+  const yesButtonRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      yesButtonRef.current.focus();
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
@@ -8,7 +16,7 @@ const Modal = ({ isOpen, onClose, onConfirm, children }) => {
       <div className="modalContent">
         {children}
         <div className="actionButtons">
-          <button onClick={onConfirm}>Yes</button>
+          <button ref={yesButtonRef} onClick={onConfirm}>Yes</button>
           <button className='cancel-button' onClick={onClose}>No</button>
         </div>
       </div>

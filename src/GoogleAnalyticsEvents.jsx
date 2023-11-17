776b52e4a64c089391283d42526181710c216749
RiOTDESiGN <125983659+erik-r-u-kodehode@users.noreply.github.com>
2023-11-17 23:58:50 +0100

    updated GoogleAnalyticsEvents

--- src/GoogleAnalyticsEvents.jsx
+++ src/GoogleAnalyticsEvents.jsx
@@ -1,9 +1,9 @@
 import { logEvent } from "firebase/analytics";
-import { analytics } from './firebase';
+import { analytics } from "./firebase";
 
 export function trackLoginEvent(user) {
-  logEvent(analytics, 'login', {
-    userId: user.uid, // User Information
-    method: 'EmailAndPassword', // Login method
+  logEvent(analytics, "login", {
+    userId: user.uid,
+    method: "EmailAndPassword",
   });
 }
